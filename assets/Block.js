95863029b6caf1510bbb2e65196d9f6c0da64b43
Matt London <mattlondon57@gmail.com>
2021-07-16 15:49:06 -0400

    Basic functionality grav/col

--- assets/Block.js
+++ assets/Block.js
@@ -1,23 +1,41 @@
 class Block {
     // ====================== Constructors ======================
-    constructor(height, width, left, top, weight, color, parent) {
-        this.height = height;
-        this.width = width;
-        this.weight = weight;
+    constructor(height, width, left, top, weight, color, parent, rigid, gravity, mobile) {
+        this.height = parseInt(height);
+        this.width = parseInt(width);
+        this.weight = parseInt(weight);
         this.color = color;
 
-        this.left = left;
-        this.top = top;
+        this.rigid = rigid;
+        this.mobile = mobile;
+        this.has_gravity = gravity;
+
+        this.left = parseInt(left);
+        this.top = parseInt(top);
 
         this.parent = parent;
 
+        // Calc bottom and right
+        this.bottom = this.top + this.height;
+        this.right = this.left + this.width;
+
+        // velocity and stuff
+        this.velocity = 0;
+        this.is_clicked = false;
+        this.collision = false;
+
         this.div = document.createElement("div");
-        dragElement(this.div);
+
+        if (this.mobile) {
+            dragElement(this.div);
+        }
         
         this.build();
 
-        // Add event listener for click to update position
-        this.div.addEventListener('click', this.update);
+        // // Add event listener for click to update position
+        // this.div.addEventListener('click', () => {
+        //     this.update();
+        // });
     }
 
     // ====================== Getters ======================
@@ -67,24 +85,58 @@ class Block {
     }
 
     // Updates position and what-not
-    update() {
-        console.log(this.div);
+    update(dt) {
         this.div.style.height = this.height + "px";
         this.div.style.width = this.width + "px";
         this.div.style.backgroundColor = this.color;
-        this.top = this.div.style.top;
-        this.left = this.div.style.left;
-        console.log({x: this.left, y: this.top});
+        this.top = parseInt(this.div.style.top.slice(0, this.div.style.top.length - 2));
+        this.left = parseInt(this.div.style.left.slice(0, this.div.style.left.length - 2));
+        this.bottom = this.top + this.height;
+        this.right = this.left + this.width;
+
+        this.update_position(dt);
+
+        
     }
 
     // Build object in dom
     build() {
+        if (this.mobile) {
+            this.div.style.cursor = "pointer";
+        }
+
         this.div.style.position = "absolute";
-        this.div.style.cursor = "pointer";
+        this.div.className = "block";
         this.div.style.top = this.top + "px";
         this.div.style.left = this.left + "px";
-        this.update();
+        this.update(time_elapsed());
         this.parent.appendChild(this.div);
+    }
+
+    // Apply gravity by calculating change in position
+    gravity(dt) {
+        // Check if it is currently in collision
+        if (!this.is_clicked && this.bottom < window.innerHeight - 10) {
+            // Calculate new velocity
+            this.velocity += g * dt;
+            // Calculate new position
+            let dy = this.velocity * dt + 0.5 * g * Math.pow(dt, 2);
+
+            this.div.style.top = (this.top - dy) + "px";
+
+        }
+
+        else {
+            this.velocity = 0;
+        }
+    }
+
+    // Apply velocity to update position
+    update_position(dt) {
+        // Run gravity
+        if (!this.collision && this.has_gravity) {
+            this.gravity(dt);
+        }
     }
 
 }
