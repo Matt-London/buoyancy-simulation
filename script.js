00a38e85aa69987cbe7820b53d076e2f2c89a1f8
Matt London <mattlondon57@gmail.com>
2021-07-25 01:39:16 -0400

    Build scene framework

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // 20 pixels = 1 cm
 const stage = document.getElementById("stage");
 
+// To hold scenes later
+let blocks = [];
+let divs = [];
+
+const scene = new Scene(stage);
+
+blocks = scene.block_array;
+divs = scene.blockDiv_array;
 
 load_ready = true;
 
 // Main "game" loop
 setInterval(() => {
-    update_blocks(time_elapsed());
+    update_scene(scene);
 
-    grab_form();
+    update_options();
 
 }, 20);
--- src/spec/options.js
+++ src/spec/options.js
@@ -4,7 +4,7 @@ const densityElement = document.getElementById("visible-density");
 const volumeElement = document.getElementById("visible-volume");
 
 // Grabs form data and saves to global variables
-function grab_form() {
+function update_options() {
     show_mass = massElement.checked;
     show_weight = weightElement.checked;
     show_density = densityElement.checked;
