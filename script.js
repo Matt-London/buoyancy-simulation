edffffa308342b03bb5f0a0b3e9ecdd4b32db148
Matt London <mattlondon57@gmail.com>
2021-07-27 13:37:20 -0400

    Simplify example

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ let blocks = [];
 let divs = [];
 
 const scene = new Scene(stage);
+scene.add_object(new Block(100, 100, 100, 100, 2, 10, true, "images/wood.jpg", stage, true, true, true, true));
+scene.add_object(new Scale(75, 150, 300, 100, stage));
 
 blocks = scene.block_array;
 divs = scene.blockDiv_array;
 
 load_ready = true;
+
+
+
+
 
 // Main "game" loop
 setInterval(() => {
